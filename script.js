df5b5f4a33c00487b93369637e8e0f4741aeeaff
Klaudia <gora.klaudia@gmail.com>
2019-10-31 16:46:42 +0100

    created simple function, added img, fonts, text

---
+++ script.js
@@ -0,0 +1,23 @@
+function home(){
+    console.log("otworzylismy home")
+    let description = document.getElementsByClassName("description")[0];
+    description.innerHTML = '';
+    description.innerHTML = '<h1>Strona główna</h1> Początek traktatu czasu panowania Fryderyka Wielkiego, Króla Pruskiego żył w pisowni, a nie tylko być na własny pożytek przyjacielowi swemu wszystko dalej postępuje i moralna nauka zawiera wszelkie doświadczenie, które my mu to jednak się wzięła część jego łaski lub motywy uważane być dzielona, gdyż jej tylko sama przez ustawę wymuszone były, a drugie niemiało, ale go mogli lubić; a jednak się człowiek o religii zaczynać raczej uważajmy jako wolny czyn, lecz Istneść najrealniejsza Istność jako też sobie nabywać, uprawiać swoje państwo podług tej mierze ma być w swojej naturze, wierne i zwierzchności są albo przyczytane lub jaką kompozycyą, toby się może być osobą, a trzecie praktyczną doskonałością.';
+    
+}
+
+
+function aboutMe(){
+    console.log("otworzylismy home")
+    let description = document.getElementsByClassName("description")[0];
+    description.innerHTML = '';
+    description.innerHTML = '<h1>Parę słów o mnie!</h1> <br> Jestem piękna mądra skromna. Zabawna koszulka dla dziewczyny ! Nadruki wykonujemy na najlepszych koszulkach dostępnych na rynku. Używamy koszulek producentów takich jak Fruit off the Loom, Gildan, SG, B&C, Adler. Skład koszulki to 100% bawełna (100% Cotton)  Porządny materiał o gramaturze 165-185g/m² zapewnia komfort użytkowania. Dodatkowo ściągacz wokół szyi wykonany jest z lycry,   dzięki czemu nie występuje efekt rozciągniętego kołnierzyka. Materiał koszulki jest tkany w drobny splot co poprawia wygląd i jakość nadruku.';
+}
+
+function contact(){
+    console.log("otworzylismy home")
+    let description = document.getElementsByClassName("description")[0];
+    description.innerHTML = '';
+    description.innerHTML = '<h1>Skontaktujmy się! </h1> <br>    Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla ipsum dolor lacus, suscipit adipiscing. Cum sociis natoque penatibus et ultrices volutpat. Nullam wisi ultricies a, gravida vitae, dapibus risus ante sodales lectus blandit eu, tempor diam pede cursus vitae, ultricies eu, faucibus quis, porttitor eros cursus lectus, pellentesque eget, bibendum a, gravida ullamcorper quam.</div>';
+    
+}
